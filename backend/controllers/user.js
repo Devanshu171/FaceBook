9758a531b503bf8e4df4258c78fdd7b09c3f8115
Devanshu171 <abhisim0098@gmail.com>
2022-09-06 23:16:25 +0530

    made userModel with mongoose

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,39 @@
 // controllers have all the call back functions for routes
+const User = require("../models/user");
 exports.home = (req, res) => {
   res.status(404).json({
     message: "something",
     error: "something",
   });
+};
+
+exports.register = async (req, res) => {
+  try {
+    const {
+      first_name,
+      last_name,
+      email,
+      password,
+      username,
+      bYear,
+      bMonth,
+      bDay,
+      gender,
+    } = req.body;
+    const user = await new User({
+      first_name,
+      last_name,
+      email,
+      password,
+      username,
+      bYear,
+      bMonth,
+      bDay,
+      gender,
+    }).save();
+    res.json(user);
+  } catch (error) {
+    // console.log("error saving user", error);
+    res.status(500).json({ message: error.message });
+  }
 };

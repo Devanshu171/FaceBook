ee0337f5dfa0b604e12f9f3ed69ece8d43c87b22
Devanshu171 <abhisim0098@gmail.com>
2022-09-08 21:31:50 +0530

    user validation

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,11 @@
 // controllers have all the call back functions for routes
+const {
+  validateEmail,
+  validateLenght,
+  validateUsername,
+} = require("../helpers/validation");
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 exports.home = (req, res) => {
   res.status(404).json({
     message: "something",
@@ -20,12 +26,52 @@ exports.register = async (req, res) => {
       bDay,
       gender,
     } = req.body;
+
+    if (!validateEmail(email)) {
+      return res.status(400).json({
+        message: "invalid email address",
+      });
+    }
+
+    const check = await User.findOne({ email });
+    if (check) {
+      return res.status(400).json({
+        message:
+          "The Email address is already exist! Try logging in or using another account",
+      });
+    }
+    // return;
+    if (!validateLenght(first_name, 3, 30)) {
+      return res.status(400).json({
+        message: "First name must be between 3 anfd 30 characters",
+      });
+    }
+
+    if (!validateLenght(last_name, 3, 30)) {
+      return res.status(400).json({
+        message: "Last name must be between 3 anfd 30 characters",
+      });
+    }
+
+    if (!validateLenght(password, 6, 40)) {
+      return res.status(400).json({
+        message: "Password must be atleat 6 characters",
+      });
+    }
+
+    const cryptedPassword = await bcrypt.hash(password, 12);
+
+    let tempUsername = first_name + last_name;
+    let newUsername = await validateUsername(tempUsername);
+
+    // return;
+
     const user = await new User({
       first_name,
       last_name,
       email,
-      password,
-      username,
+      password: cryptedPassword,
+      username: newUsername,
       bYear,
       bMonth,
       bDay,
--- backend/helpers/validation.js
+++ backend/helpers/validation.js
@@ -1,3 +1,35 @@
+const User = require("../models/user");
+
 exports.validateEmail = (email) => {
-  return String(email).toLowerCase().match();
+  // regex syntex  "/^" start with and "$/" end with this
+  // to divide in between sections like "()"
+  // to specify what is allowed in a section
+  // ( all this before @ a-z are charachres
+  // /d means digits
+  // for . we cant just use ""." because in regex "." means any this is allowed se ew use "/."
+  // "-" is also allowed
+  //  + means as many chars)
+  return String(email)
+    .toLowerCase()
+    .match(/^([a-z\d\.-]+)@([a-z\d\-]+)\.([a-z]{2,12})(\.[a-z]{2,12})?$/);
+};
+
+exports.validateLenght = (text, min, max) => {
+  if (text.length > max || text.length < min) return false;
+  return true;
+};
+
+exports.validateUsername = async (username) => {
+  a = false;
+  do {
+    let check = await User.findOne({ username });
+    if (check) {
+      // change username
+      username += (new Date() * Math.random()).toString().substring(0, 1);
+      a = true;
+    } else {
+      a = false;
+    }
+  } while (a);
+  return username;
 };
